ece4eeffdad40798318f57bda624e829f834b627
sochin-bhattacharjee <sochin.cs@gmail.com>
2025-01-01 20:17:49 +0600

    add cover page in menu section

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-
+import Cover from '../../shared/Cover/Cover';
+import PopularMenu from './../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
+    const menuImg = 'https://media.istockphoto.com/id/466771930/photo/table-setting.jpg?s=612x612&w=0&k=20&c=0jAnmMHcNJR7eNw5wSvnmXJdLwINcWE6icsC8t4KgBM=';
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImg} tittle="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/pages/shared/Cover/Cover.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+const Cover = ({ img, tittle }) => {
+  return (
+    <Parallax
+      blur={{ min: -50, max: 50 }}
+      bgImage={img}
+      bgImageAlt="the menu"
+      strength={-200}
+    >
+      <div
+        className="hero h-[600px] object-cover"
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{tittle}</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+  );
+};
+
+export default Cover;
